2650b48a92f3d26bcc58ff1a070293ba3213f51f
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-01-08 15:45:36 +0530

    Implemeneted the task3 in BankAccount.cs file

---
+++ OOPs/BankAccount.cs
@@ -0,0 +1,60 @@
+﻿
+    public abstract class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        protected decimal Balance { get; set; } = 0;
+    protected BankAccount(string accountNumber,decimal balance)
+    {
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+    public  void Deposit(decimal amount)
+    {
+        Balance = Balance + amount;
+    }
+    public abstract void Withdraw(decimal amount);
+    }
+public class SavingsAccount : BankAccount
+{
+    private const int MIN_BALANCE = 1000;
+    public SavingsAccount(string accountNumber, decimal balance) : base(accountNumber, balance)
+    {
+        
+    }
+   
+
+    public override void Withdraw(decimal amount)
+    {
+        if((Balance-amount)<MIN_BALANCE)
+        {
+            Console.WriteLine("Unable to dispense amount due to Minimum balance!! ");
+            return;
+        }
+        Balance -= amount;
+        Console.WriteLine("Amount dispensed successfully !!! ");
+
+    }
+}
+public class CheckingAccount : BankAccount
+{
+    
+    public CheckingAccount(string accountNumber,decimal balance):base(accountNumber,balance) 
+    {
+       
+    }
+   
+
+    public override void Withdraw(decimal amount)
+    {
+        if ((Balance - amount) < 0)
+        {
+            Console.WriteLine("Insufficient Balance !! ");
+            return;
+        }
+        Balance -= amount;
+        Console.WriteLine("Amount dispensed successfully !!! ");
+
+    }
+}
+
+
--- OOPs/Program.cs
+++ OOPs/Program.cs
@@ -7,5 +7,11 @@ using OOPs;
 //rect.PrintDetails();
 
 //Task2
-Employee employee = new Developer("prasath",1000);
-employee.printDetails();
+//Employee employee = new Developer("prasath",1000);
+//employee.printDetails();
+
+//Task3
+BankAccount account = new SavingsAccount("3746347",1200);
+account.Deposit(100); //   Deposit success - balance=1300
+account.Withdraw(300);//Withdram Sucess - balance =1000
+account.Withdraw(1); //Withdram fails because of mininum balance
