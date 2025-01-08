5161a81056a41280270ba22bab4f642e9a2c05a9
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-01-08 15:06:10 +0530

    Implemented the task2 in the Employee.cs

---
+++ OOPs/Employee.cs
@@ -0,0 +1,46 @@
+﻿ public abstract  class Employee
+  {
+    public int BonusPercentage { get; init; }
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+    protected Employee(string name ,decimal salary)
+    {
+       
+        Name = name;
+        Salary = salary;
+    }
+
+    public abstract decimal calculateBonus();
+    public void printDetails()
+    { 
+        Console.WriteLine($"Details :\nName :{Name}\nSalary :{Salary}\nBonus:{calculateBonus()}");
+
+    }
+
+    }
+public class Manager:Employee
+{
+    
+    public Manager(string name,decimal salary):base( name,salary)
+    {
+        BonusPercentage = 40;
+    }
+    public override decimal calculateBonus()
+    {
+        
+        return (decimal)(base.Salary * BonusPercentage)/100;
+    }
+}
+public class Developer : Employee
+{
+    
+    public Developer(string name, decimal salary) : base(name, salary)
+    {
+        BonusPercentage = 30;
+    }
+    public override decimal calculateBonus()
+    {
+        return (decimal)(base.Salary * BonusPercentage) / 100;
+    }
+}
+
--- OOPs/Program.cs
+++ OOPs/Program.cs
@@ -2,6 +2,10 @@
 using OOPs;
 //Task1
 
-Shape rect = new Rectangle(10, 5, "red");
-rect.CalculateArea();
-rect.PrintDetails();
+//Shape rect = new Rectangle(10, 5, "red");
+//rect.CalculateArea();
+//rect.PrintDetails();
+
+//Task2
+Employee employee = new Developer("prasath",1000);
+employee.printDetails();
