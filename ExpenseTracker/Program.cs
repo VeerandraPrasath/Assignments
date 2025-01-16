463de5d39cbee28efac44590fcee71c5e5cba650
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-01-16 14:10:25 +0530

    modified project folder

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,10 +1,2 @@
-﻿namespace ExpenseTracker
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
