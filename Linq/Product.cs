6800738796e2a15c468115978ac760b1f7c77985
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-02-03 16:08:52 +0530

    Created product model

---
+++ Linq/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Linq
+{
+    public class Product
+    {
+        public string Name { get; set; }    
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+            
+        }
+    }
+}
