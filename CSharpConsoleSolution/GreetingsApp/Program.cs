58592e0583fd0c4fbac617e60248aaa9a0e7343b
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-02-26 19:20:16 +0530

    Implemented geeting message in GreetingsApp

--- CSharpConsoleSolution/GreetingsApp/Program.cs
+++ CSharpConsoleSolution/GreetingsApp/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace GreetingsApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("**************************************");
+            Console.WriteLine("*                                    *");
+            Console.WriteLine("*                                    *");
+            Console.WriteLine("*   Welcome to the Greetings App !   *");
+            Console.WriteLine("*                                    *");
+            Console.WriteLine("*                                    *");
+            Console.WriteLine("**************************************");
         }
     }
 }
