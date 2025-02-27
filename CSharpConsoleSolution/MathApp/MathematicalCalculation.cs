d99cc247b391012c1376a1bba1c6a893f2fbdfb7
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-02-27 10:19:29 +0530

    Added projectE and Documented in README.md file

--- CSharpConsoleSolution/MathApp/MathematicalCalculation.cs
+++ CSharpConsoleSolution/MathApp/MathematicalCalculation.cs
@@ -2,9 +2,15 @@
 
 namespace MathApp
 {
+    /// <summary>
+    /// Implements mathematical calculation
+    /// </summary>
     public class MathematicalCalculation
-    { 
-        public  void PerformMathematicalCalculation()
+    {
+        /// <summary>
+        /// Perform Mathematical calculations
+        /// </summary>
+        public void PerformMathematicalCalculation()
         {
             bool isExit = false;
             Console.Write("Enter number 1 :");
@@ -13,22 +19,22 @@ namespace MathApp
             int num2 = Helper.GetInt();
             while (!isExit)
             {
-                int ans=0;
+                int ans = 0;
                 Console.WriteLine("\n[1] Add\n[2] Subtract\n[3] Multiply\n[4] Divide\n[5] Exit\nEnter the choice :");
                 string userChoice = Helper.GetString();
                 switch (userChoice)
                 {
                     case "1":
-                       ans=Add(num1, num2);
+                        ans = Add(num1, num2);
                         break;
                     case "2":
-                       ans= Subtract(num1, num2);
+                        ans = Subtract(num1, num2);
                         break;
                     case "3":
-                       ans= Multiply(num1, num2);
+                        ans = Multiply(num1, num2);
                         break;
                     case "4":
-                       ans= Divide(num1, num2);
+                        ans = Divide(num1, num2);
                         break;
                     case "5":
                         isExit = true;
@@ -37,7 +43,7 @@ namespace MathApp
                         Console.WriteLine("Invalid choice !");
                         break;
                 }
-                Console.WriteLine("Answer is "+ans);
+                Console.WriteLine("Answer is " + ans);
             }
         }
 
