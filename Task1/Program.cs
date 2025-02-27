71883aae83b8161bb5dfa885a32508017ec76b2c
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-02-27 14:34:21 +0530

    Implemented Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment16.Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            notifier.OnAction += MessageHandler;
+            notifier.PerformAction("Action performed successfully!");
+            Console.ReadLine();
+        }
+        static void MessageHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Notifier
+    {
+        public delegate void Notify(string message);
+
+        public event Notify OnAction;
+
+        public void PerformAction(string actionMessage)
+        {
+            OnAction?.Invoke(actionMessage);
+        }
+    }
+
+}
