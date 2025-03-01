c9c6e9cebb9c8ce853d88147850e7bf786b47952
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-01 11:19:30 +0530

    Implemented Task1

---
+++ Reflection/AssemblyModel/Class2.cs
@@ -0,0 +1,22 @@
+﻿namespace AssemblyModel
+{
+    public class Class2
+    {
+        private string _field2;
+
+        public string Property2 {  get; set; }  
+
+        public event EventHandler AnotherEvent;
+
+        public void Method3()
+        {
+            Console.WriteLine("Method3 of Class2");
+        }
+
+
+        //public void TriggerEvent()
+        //{
+        //    AnotherEvent?.Invoke(this, EventArgs.Empty);
+        //}
+    }
+}
