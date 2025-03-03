0b920efbce486b95f71da3d30b1117e43ada0c34
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-03 17:50:24 +0530

    Implemented Task7

---
+++ Reflection/Task7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Prasath", 25, new List<string> { "Reading", "Traveling", "Swimming" });
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            SimpleSerializer simpleSerializer = new SimpleSerializer();
+            string simpleSerialized = simpleSerializer.Serialize(person);
+            Console.WriteLine("Simple serialize :\n"+simpleSerialized);
+            stopwatch.Stop();
+            Console.WriteLine("Simple serialize time : " + stopwatch.ElapsedMilliseconds + "ms");
+            stopwatch.Restart();
+            stopwatch.Start();
+            var serializer = new EmitSerializer();
+            string emitSerialized = serializer.Serialize(person);
+            Console.WriteLine("Emit serialize :\n"+emitSerialized);
+            stopwatch.Stop();
+            Console.WriteLine("Emit serialize time : " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Hobbies { get; set; }
+
+        public Person(string name, int age, List<string> hobbies)
+        {
+            Name = name;
+            Age = age;
+            Hobbies = hobbies;
+        }
+    }
+
+}
---
+++ Reflection/Task7/SimpleSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Collections;
+
+namespace Task7
+{
+    public class SimpleSerializer
+    {
+        public string Serialize(object obj)
+        {
+            if (obj == null) return "null";
+
+            Type type = obj.GetType();
+            if (type.IsPrimitive || obj is string)
+            {
+                return JsonSerializer.Serialize(obj);
+            }
+
+            if (obj is IEnumerable enumerable)
+            {
+                var list = new List<string>();
+                foreach (var item in enumerable)
+                {
+                    list.Add(Serialize(item));
+                }
+                return "[" + string.Join(",", list) + "]";
+            }
+
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var dictionary = new Dictionary<string, string>();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(obj);
+                dictionary[property.Name] = Serialize(value);
+            }
+
+            return JsonSerializer.Serialize(dictionary);
+        }
+    }
+
+}
