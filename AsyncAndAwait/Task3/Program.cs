9e0c7b0d96246a39461e7c13f55aed52b86a7614
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-04 17:58:00 +0530

    Implemented Task3

---
+++ AsyncAndAwait/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Headers;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            int val1 = 0, val2 = 0, val3 = 0;
+            Thread thread1 = new Thread(() => { val1 = AverageOfArray(array); });
+            Thread thread2 = new Thread(() => { val2 = FirstElement(array); });
+            Thread thread3 = new Thread(() => { val3 = SumOfElement(array); });
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            Console.WriteLine("Average = " + val1 + "  FirstElement = " + val2 + "   Sum of element =" + val3);
+        }
+
+        private static int AverageOfArray(int[] array)
+        {
+            return (int)array.Average(x => x);
+        }
+
+        private static int FirstElement(int[] array)
+        {
+            return array.FirstOrDefault();
+        }
+
+        private static int SumOfElement(int[] array)
+        {
+            return array.Sum(x => x);
+        }
+    }
+}
