42197da9ff92a9f0ffe1ae625ae59d97fa8c45bb
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-04 16:42:32 +0530

    Implemented Task2

---
+++ AsyncAndAwait/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i+1;
+            }
+
+            Parallel.ForEach(arr, (i) =>
+            {
+                Console.WriteLine(i * i);
+            });
+            Console.WriteLine("Done");
+        }
+    }
+}
