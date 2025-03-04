c06afe5b4702a0a0189b348af585efad08be8c21
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-04 16:25:05 +0530

    Implemented Task1

---
+++ AsyncAndAwait/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        const string URL = "https://opentdb.com/api.php?amount=10&category=17&difficulty=easy";
+
+        private static void Main(string[] args)
+        {
+            Task<string> task = GetDataFromURL(URL);
+            string data = task.Result;
+            Console.WriteLine(data);
+        }
+
+        private static async Task<string> GetDataFromURL(string uri)
+        {
+            string responseBody = await client.GetStringAsync(uri);
+
+            return responseBody;
+        }
+    }
+}
