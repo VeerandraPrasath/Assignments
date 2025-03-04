999a0b91901ff425ed738f4d43ce420b9088a6c6
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-04 11:38:48 +0530

    Resolve reviews

--- CSharpConsoleSolution/DisplayApp/Program.cs
+++ CSharpConsoleSolution/DisplayApp/Program.cs
@@ -5,12 +5,7 @@ namespace DisplayApp
     {
         static void Main(string[] args)
         {
-            //Reason in the REFLECTION.md file
             Console.WriteLine("Cannot use the classes of the MathApp !");
-
-            //Console.WriteLine("Using MathApp classes inside DispayApp");
-            //MathematicalCalculation mathematicalCalculation = new MathematicalCalculation();
-            //mathematicalCalculation.PerformMathematicalCalculation();
         }
     }
 }
