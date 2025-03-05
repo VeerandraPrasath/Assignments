b10fe3b1ffcfab09e4875b7acf7a6ed4eb99cc0c
VeerandraPrasath <veerandra.prasath@solitontech.com>
2025-03-05 15:49:55 +0530

    Implemented Task5

---
+++ AsyncAndAwait/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ModifiedCode modifiedCode = new ModifiedCode();
+            Thread thread1 = new Thread(modifiedCode.Method1);
+            Thread thread2 = new Thread(modifiedCode.Method2);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AsyncAndAwait/Task5/StarterCode.cs
@@ -0,0 +1,36 @@
+﻿namespace Task5
+{
+    /// <summary>
+    /// Given starter code
+    /// </summary>
+    public class StarterCode
+    {
+        AutoResetEvent event1 = new AutoResetEvent(false);
+
+        /// <summary>
+        /// Method 1
+        /// </summary>
+        public void Method1()
+        {
+            lock (this)
+            {
+                Console.WriteLine("Thread1 Entering method1");
+                event1.WaitOne();
+                Console.WriteLine("Released event1");
+                Console.WriteLine("THREAD Exiting method1");
+            }
+        }
+
+        /// <summary>
+        /// Method 2
+        /// </summary>
+        public void Method2()
+        {
+            lock (this)
+            {
+                Console.WriteLine("Thread 2 Entering method2");
+                Console.WriteLine("Thread Exiting method 2");
+            }
+        }
+    }
+}
